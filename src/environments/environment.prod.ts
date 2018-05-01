ce1d84df91e7cd43a92b914a64f8ae35ff7c5326
Denys Markov <denys.markov@masterofcode.com>
2018-05-01 11:44:07 +0300

    updated prod env file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+    production: true,
+    API_URL: 'https://6umygmht58.execute-api.eu-central-1.amazonaws.com',
+    API_KEY: 'UQOGobPQyq8vA1QLbgeoZ1mjOM7XJdi6C9ymnS92'
 };
